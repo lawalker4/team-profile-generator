8f45a0069bdfa6ccb11e579407a6829d93067b38
LA WALKER <lauraashleywalker4@gmail.com>
2022-05-08 16:16:27 -0400

    add inquirei questions

--- index.js
+++ index.js
@@ -41,10 +41,114 @@ function addCreateTeamList() {
 }
 
 //Create functions to add engineer, interns and managers
+function addEngineer(){
+    console.log(`
+    =======================
+    Team Profile Generator!
+    =======================
+    `);
+    return inquire
+    .prompt([
+        {
+            type:"input",
+            name: "engineerName",
+            message: "Engineers Name"
+        },
+        
+        {
+            type:"input",
+            name: "engineerID",
+            message: "The Engineers Employee ID Number"
+        },
+        {
+            type:"input",
+            name: "engineerEmail",
+            message: "The Engineers Email Address"
+        },
+        {
+            type:"input",
+            name: "engineerGitHub",
+            message: "The Engineers GitHub"
+        },
+    ]).then(function(data){
+        const engineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+        team.push(engineer);
+        addCreateTeamList
+    });
+}
 
+function addIntern(){
+    console.log(`
+    ==============
+    Add New Intern
+    ==============
+    `)
 
+    return inquire
+    .prompt([
+        {
+            type:"input",
+            name: "internName",
+            message: "Intern Name"
+        },
+        
+        {
+            type:"input",
+            name: "internID",
+            message: "The Interns Employee ID Number"
+        },
+        {
+            type:"input",
+            name: "internEmail",
+            message:"Interns Email Address"
+        },
+        {
+            type:"input",
+            name: "internSchool",
+            message: "The name of the school the intern is from."
+        },
+    ]).then(function(data){
+        const intern = new Intern(data.interName, data.internId, data.internEmail, data.internSchool);
+        team.push(intern);
+        addCreateTeamList
+    });
+}
 
-
+function addManager(){
+    console.log(`
+    ================
+    Add New Manager
+    ================
+    `)
+    return inquire
+    .prompt([
+        {
+            type:"input",
+            name: "managerName",
+            message: "Manager Name"
+        },
+        
+        {
+            type:"input",
+            name: "managerID",
+            message: "Manager Employee ID Number"
+        },
+        {
+            type:"input",
+            name: "managerEmail",
+            message: "Manager Email Address"
+        },
+        {
+            type:"input",
+            name: "managerOfficeNumber",
+            message: "Manager Office Number"
+        },
+    ]).then(function(data){
+        const manager = new Manager(data.managerame, data.managerId, data.managerEmail, data.managerOfficeNumber);
+        team.push(manager);
+        addCreateTeamList
+    }); 
+}
 
 
 
