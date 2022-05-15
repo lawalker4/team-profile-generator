40e23bbe79781dbb64721c1db943094bf633905d
LA WALKER <lauraashleywalker4@gmail.com>
2022-05-15 12:46:33 -0400

    final push

--- templates/pagetemplate.js
+++ templates/pagetemplate.js
@@ -89,7 +89,7 @@ module.exports = (newTeam) => {
             <script src="https://kit.fontawesome.com/58bfb9f4e3.js" crossorigin="anonymous"></script>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-            <title>The Team Profile Gnerator</title>
+            <title>The Team Profile Generator</title>
             </head>
             <body>
                 <header>
